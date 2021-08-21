edeb2290444a83bc24e7ac06ee30bde28fd34827
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-22 00:55:46 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,342 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Silicone Hot Skillet Handle',
+        price: 5.95,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Insulating kitchen accessory protects hands from hot pan handles.',
+            },
+            {
+                text: 'High-quality, heat-resistant silicone creates insulating layer of protection; oven safe up to 475 Degrees F/250 Degrees C.',
+            },
+            {
+                text: 'Works with a variety of cookware handles ranging from 10.5-13.25 inches long, including most cast iron skillets.',
+            },
+            {
+                text: 'DO NOT use holder when coming in contact with open flames, leave the holder on the handle while cooking in the oven, or use with temperatures higher than 250 degrees C/475 degrees F for longer than 1 hour.',
+            },
+            {
+                text: 'DO use as a guard to protect your hands from burns, use the anti-slip grip to slide the holder onto a hot handle, and use to transfer hot items to and from the oven.',
+            },
+            {
+                text: 'Slides on and off easily and stays securely in place while in use; measures 5.6 inches long x 1.9 inches wide; fits skillets with handles 10.5 inches or larger.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.jpg'],
+    },
+    {
+        id: 'product-2',
+        name: 'Iron Brush and Scraper',
+        price: 19.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Ultra Tough Wide-Head Brush Safely Cleans Cast Iron Cookware.',
+            },
+            {
+                text: 'Built-In Scraper Removes Even the Most Stuck-On Bits.',
+            },
+            {
+                text: 'Material type: Bamboo recycled plastic.',
+            },
+            {
+                text: 'A Non-toxic BPA Free Coating.',
+            },
+            {
+                text: 'Small footprint at the sink and on the planet.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Kids Bedding Nap Set',
+        price: 24.99,
+        discount: 14.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: '100% Polyester.',
+            },
+            {
+                text: 'Kids throw blanket and pillow set for naps, travel, and more.',
+            },
+            {
+                text: 'Mix and match with a variety of bedding, bath, storage and decor products.',
+            },
+            {
+                text: 'Kid-friendly designs for a fun, whimsical experience.',
+            },
+            {
+                text: 'Plush 100% polyester construction.',
+            },
+            {
+                text: 'All-season warmth and breathability.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.jpg', './img/img3-4.jpg'],
+    },
+    {
+        id: 'product-4',
+        name: 'Ironing Board',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                title: 'Expandable steel legs',
+                text: 'The metal legs expand up to 25” wide to ensure lockdown stability to prevent wobbling while the orange non-skidded feet protect your wood or tile flooring.',
+            },
+            {
+                title: '54” x 14.875” PERFORATED STEEL TOP',
+                text: 'Offers a large ironing surface and evenly distributes heat and steam onto garments, eliminating wrinkles effortlessly.',
+            },
+            {
+                title: '100% cotton cover',
+                text: 'A smooth cotton cover with enhanced padding made of ¼” foam and ¼” fiber pad provides additional thickness and allows your iron to glide across your garments and leave them wrinkle free.',
+            },
+            {
+                title: 'Adjusts to any height',
+                text: 'Conveniently adjust to any height, up to 39” tall.',
+            },
+            {
+                title: 'Folds flat',
+                text: 'A large release lever with whisper-glide technology makes for easy set up and storage.',
+            },
+            {
+                title: 'Built-in iron rest',
+                text: 'The iron rest contains silicon pads to disperse heat when the steam iron is set aside and includes hanger slots to hang freshly pressed garments.',
+            },
+            {
+                title: 'Neutral color pallet',
+                text: 'Includes a gray cover with a gray colored frame and gray accents to compliment any laundry room.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-4.png', './img/img4-5.png', './img/img4-6.png', './img/img4-7.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Plastic Infant Swing',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Heavy-duty plastic is safe and sturdy with a high back that protects your infant’s back and neck.',
+            },
+            {
+                text: 'Recommended for children as young as 7 months and up to 3 years of age.',
+            },
+            {
+                text: 'Adjustable belt and T-Bar ensure your little one is carefully secure for your peace of mind.',
+            },
+            {
+                text: 'Durable nylon rope can be adjusted for hanging.',
+            },
+            {
+                text: 'Supports a weight limit of up to 35 pounds.',
+            },
+        ],
+        images: ['./img/img5-1.jpg', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Corded Knife & Tool Sharpener',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Sharpen all your knives and tools with one unit.',
+            },
+            {
+                text: 'Knife guides fixed at 20 degrees, Blade guides hold knife at correct sharpening angle.',
+            },
+            {
+                text: 'Floating backing plate allows belt to stay straight and does not burn or cut the tip of a knife.',
+            },
+            {
+                text: 'Includes 3 replacement belts: fine, medium and coarse.',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png', './img/img6-3.png', './img/img6-4.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Set of 2 Metal Bird Cage',
+        price: 49.95,
+        discount: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: 'The vintage design of this home décor brings a sense of the past to your home.',
+            },
+            {
+                text: 'This item is made of solid iron material for durability and long lasting.',
+            },
+            {
+                text: 'This elegant set of decorative birdcages feature a distressed cream iron finish.',
+            },
+            {
+                text: 'This set features dome-shaped bird cages that look great on tables or ceilings.',
+            },
+            {
+                text: 'Each item in this set measures 7”L x 7”W x 18”H, 9”L x 9”W x 21”H, weight 4.41 lbs.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Tour Bag',
+        price: 69.99,
+        discount: 69.94,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Roomy compartment withzippered RFID blocking pocket,padded pocket to hold tablet, and tethered key clipwith LED light.',
+            },
+            {
+                text: 'Two easy access, locking front pockets.',
+            },
+            {
+                text: 'Two mesh expansion pockets hold a water bottle, umbrella or sunglasses. Rear zippered pocket. hold a water bottle, umbrella or sunglasses.',
+            },
+            {
+                text: 'Material: Cotton Polyester Blend. Size: 9" x 11.5" x 3". Strap Drop Length: 14" – 25"',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png', './img/img8-3.png', './img/img8-4.png', './img/img8-5.png', './img/img8-6.png', './img/img8-7.png', './img/img8-8.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Dual Node Massager with Heat',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                title: 'Dual head percussion',
+                text: 'The Dual Node Massager offers a stimulating massage experience for the entire body. The massager offers different speed options, three sets of nodes, and soothing heat for optimal performance. Use for your neck, back, and shoulders.',
+            },
+            {
+                title: 'Soothing heat',
+                text: 'Enhance your massage experience with the optional gentle heat setting. The soothing heat will comfort your tired and overworked muscles for added relaxation, and help loosen tense muscles.',
+            },
+            {
+                title: 'Customized settings',
+                text: 'The massager offers three speed settings that allow you to enjoy a gentle or a more intense massage. Three sets of massaging nodes are includes for added customization of your massage experience.',
+            },
+            {
+                title: 'Comfort design',
+                text: 'The rubberized design makes the handle easy to grip and guide the massager. The front comfort-grip handle is great for reaching all those hard-to-reach areas for the ultimate massage experience.',
+            },
+            {
+                title: 'Homedics',
+                text: 'Dual Node Massager with Heat is about creating a healthy home environment that helps you relax your body, de-stress, and simplify your life.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png', './img/img9-4.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Mini Stepper with Resistance Bands',
+        price: 84.41,
+        discount: 95.50,
+        image: './img/img10.png',
+        description: [
+            {
+                title: 'Track your fitness',
+                text: 'LCD monitor measures the steps time total and calories, making your exercise more reasonable and effective.',
+            },
+            {
+                title: 'Total body',
+                text: 'Equipped with removable training bands this high-quality stepper tightens over 20 different core muscles and using low-impact aerobic exercise to help tone your chest back and shoulders.',
+            },
+            {
+                title: 'Hydraulic drive system',
+                text: 'Built with a hydraulic drive system this workout machine provides a smooth stepping motion. Arrives assembled (simply attach resistance bands).',
+            },
+            {
+                title: 'Height adjustment',
+                text: 'Height adjustment knob adjusts the step motion height for either short bursts or long steps.',
+            },
+            {
+                title: 'Wide non-slip pedals',
+                text: 'Large textured foot plates ensure a solid, non-slip footing Height range will decrease with each counter-clockwise turn.',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.jpg', './img/img10-3.jpg', './img/img10-4.png', './img/img10-5.png', './img/img10-6.png', './img/img10-7.png', './img/img10-8.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Hand Vacuum',
+        price: 89.99,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                text: 'Lithium Ion for long battery life and outstanding performance. Always ready - holds a charge for up to 18 months.',
+            },
+            {
+                text: 'Long life, Lightweight, and no memory effect.Suction Power-15.2 AW.',
+            },
+            {
+                text: 'Smart Charge Technology uses up to 50% less energy,air Watts: 15.2 Watts, Voltage: 16V MAX.',
+            },
+            {
+                text: 'Cyclonic action helps to keep the filter clean and power strong, translucent bagless dirt bowl easy to see dirt and empty.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png', './img/img11-4.png', './img/img11-5.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Indoor Low Profile ceiling Fan',
+        price: 109.00,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                title: 'Classic ceiling fan',
+                text: 'The traditional fan comes with LED light covered by swirled marble glass that will keep home interior current and inspired; Measures 42 x 42 x 14.32 Inch.',
+            },
+            {
+                title: 'Multi-speed reversible fan motor',
+                text: 'Wind motor delivers ultra-powerful airflow with quiet performance; Change the direction from downdraft mode during the summer to updraft mode during the winter.',
+            },
+            {
+                title: 'LED bowl light kit',
+                text: 'Energy-efficient dimmable LED light bulbs let you control the lighting and ambiance of the living space; The long lasting bulbs have longer lifespan than traditional bulbs.',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.png', './img/img12-3.jpg', './img/img12-4.png', './img/img12-5.png', './img/img12-6.png'],
+    },
+    {
+        id: 'product-13',
+        name: 'Piano for kids',
+        price: 119.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                text: 'Piano produces grand sound to accompany its grand appearance.',
+            },
+            {
+                text: 'This piano has a realistic look, excellent resonant sound, and a lid that can be opened.',
+            },
+            {
+                text: 'Promotes dexterity, hand-eye coordination, musical skills, and creativity.',
+            },
+            {
+                text: 'Recommened for ages 36 months and up.',
+            },
+            {
+                text: 'Non-toxic finishes and child safe materials.',
+            },
+        ],
+        images: ['./img/img13-1.png', './img/img13-2.png', './img/img13-3.png'],
+    },
+]
